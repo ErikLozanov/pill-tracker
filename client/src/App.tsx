e4a26ee2c11e39cf5e56355f6924368979fb24cf
Erik Lozanov <122800063+ErikLozanov@users.noreply.github.com>
2025-12-06 18:10:58 +0200

    initalized vite + tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-xl shadow-lg text-center">
+        <h1 className="text-3xl font-bold text-blue-600 mb-2">
+          It Works! 💊
+        </h1>
+        <p className="text-gray-500">
+          Tailwind is active.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
